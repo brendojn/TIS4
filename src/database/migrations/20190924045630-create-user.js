a560b0fe5ff6eff875de7100129ac512166afb89
victorseara <victorgoestocollege@gmail.com>
2019-09-24 02:30:44 -0300

    user migration

---
+++ src/database/migrations/20190924045630-create-user.js
@@ -0,0 +1,58 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      social_id: {
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      profile: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      profile_img: {
+        type: Sequelize.STRING,
+      },
+      header_img: {
+        type: Sequelize.STRING,
+      },
+      dt_birth: {
+        type: Sequelize.DATE,
+      },
+      gender: {
+        type: Sequelize.STRING,
+      },
+      last_sign_in: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  },
+};
