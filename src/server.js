878066eaa571c7d3cf510bc44b1fe0576c573886
victorseara <victorgoestocollege@gmail.com>
2019-09-24 02:30:26 -0300

    db config and app structure boilerplate

--- src/server.js
+++ src/server.js
@@ -1,19 +1,3 @@
-require('console-stamp')(console, '[HH:MM:ss.l]');
+const app = require('./app');
 
-const fs = require('fs');
-const cors = require('cors');
-const express = require('express');
-const bodyParser = require('body-parser');
-const { router } = require('./api');
-
-const server = express();
- 
-server.set('port', (process.env.PORT || 3000));
-
-server.use(cors());
-server.use(express.json());
-server.use('/api', router);
-
-server.listen(server.get('port'), function () {
-	console.log('Node server is running on port:', server.get('port'));
-});
+app.listen(3333);
