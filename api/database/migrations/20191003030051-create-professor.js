8f4e650aa30d41670faf6b8f32f5821eaa33cd7d
victorseara <victorgoestocollege@gmail.com>
2019-10-04 01:28:46 -0300

    Professor schema validate on store and update

--- api/database/migrations/20191003030051-create-professor.js
+++ api/database/migrations/20191003030051-create-professor.js
@@ -12,6 +12,7 @@ module.exports = {
         },
         name: {
           type: Sequelize.STRING,
+          allowNull: false,
         },
         email: {
           type: Sequelize.STRING,
--- api/src/controllers/ProfessorController.js
+++ api/src/controllers/ProfessorController.js
@@ -1,8 +1,28 @@
+import * as Yup from 'yup';
 import Professor from '../models/professor';
 import Password from '../middlewares/auth/password';
 
 class ProfessorController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      admin: Yup.boolean(),
+      profile_img: Yup.string(),
+      password: Yup.string()
+        .required()
+        .min(8),
+      registration: Yup.number()
+        .integer()
+        .positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const alreadyExists = await Professor.findOne({
       where: { email: req.body.email },
     });
@@ -24,6 +44,26 @@ class ProfessorController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      admin: Yup.boolean(),
+      profile_img: Yup.string(),
+      registration: Yup.number()
+        .integer()
+        .positive(),
+      oldPassword: Yup.string().min(8),
+      password: Yup.string()
+        .min(8)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { email, oldPassword } = req.body;
 
     const professor = await Professor.findByPk(req.userId);
@@ -35,7 +75,9 @@ class ProfessorController {
       });
 
       if (alreadyExists) {
-        return res.status(402).json({ error: 'This user already exists.' });
+        return res
+          .status(402)
+          .json({ error: 'This email belongs to another user.' });
       }
     }
 
